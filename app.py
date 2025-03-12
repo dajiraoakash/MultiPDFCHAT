553cfe796bcb5fa86ccac1fb8cea80a8484ea17a
dajiraoakash <dajiraoakash@gmail.com>
2025-03-11 20:03:09 -0400

    created GUI

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+import streamlit as st
+
 def main():
-    print("Hello World")
+    st.set_page_config(page_title="Intelligent Document Query System", page_icon=":books:")
+
+    st.header(":books: Intelligent Document Query System :books:")
+    st.text_input("Ask a question about the documents")
+    
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upoad your PDFs here and click on 'Process'")
+        st.button("Process")
 
 
 
